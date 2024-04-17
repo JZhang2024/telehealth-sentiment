282f41929af56886c223c0e597708b4c6977e841
Arvind Kasiliya <arvind.kasiliya@uconn.edu>
2024-04-17 18:08:21 -0400

    Tweak navbar a bit

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -10,36 +10,37 @@ defineProps<{
   <div class="container mx-auto px-5">
     <nav class="flex space-x-10 font-medium justify-between py-12 items-center">
       <div>
-                <a href="/"><img src="../assets/cigna.png" alt="Site Logo"></a>
+        <RouterLink to="/"><img src="../assets/cigna.png" alt="Site Logo" /></RouterLink>
       </div>
-      <RouterLink
-        to="/"
-        :class="{
-          'text-foreground': currentSection === 'home',
-          'text-muted-foreground': currentSection !== 'home'
-        }"
-        class="uppercase transition-colors hover:text-foreground">
-        Home
-      </RouterLink>
-      <RouterLink
-        to="/about"
-        :class="{
-          'text-foreground': currentSection === 'about',
-          'text-muted-foreground': currentSection !== 'about'
-        }"
-        class="uppercase transition-colors hover:text-foreground">
-        About
-      </RouterLink>
-      <RouterLink
-        to="/contact"
-        :class="{
-          'text-foreground': currentSection === 'contact',
-          'text-muted-foreground': currentSection !== 'contact'
-        }"
-        class="uppercase transition-colors hover:text-foreground">
-        Contact
-      </RouterLink>
-      
+      <div class="space-x-10">
+        <RouterLink
+          to="/"
+          :class="{
+            'text-foreground': currentSection === 'home',
+            'text-muted-foreground': currentSection !== 'home'
+          }"
+          class="uppercase transition-colors hover:text-foreground">
+          Home
+        </RouterLink>
+        <RouterLink
+          to="/about"
+          :class="{
+            'text-foreground': currentSection === 'about',
+            'text-muted-foreground': currentSection !== 'about'
+          }"
+          class="uppercase transition-colors hover:text-foreground">
+          About
+        </RouterLink>
+        <RouterLink
+          to="/contact"
+          :class="{
+            'text-foreground': currentSection === 'contact',
+            'text-muted-foreground': currentSection !== 'contact'
+          }"
+          class="uppercase transition-colors hover:text-foreground">
+          Contact
+        </RouterLink>
+      </div>
     </nav>
   </div>
 </template>
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -30,24 +30,29 @@ function handleJoin() {
 </script>
 
 <template>
-	<Navbar currentSection="home" />
-  <div class="container mx-auto px-5 overflow-x-hidden lg:overflow-x-visible">
-      <section class="grid grid-cols-none lg:grid-cols-2 pb-16 pt-8 items-center">
-          <div class="lg:w-5/6 order-2 lg:order-none">
-              <h1 class="text-4xl xl:text-5xl font-bold font-theme-heading text-center lg:text-left">Welcome to Cigna Telehealth!</h1>
-              <p class="lg:text-xl mt-10 font-theme-content text-theme-grayish-blue text-center lg:text-left">A communication tool for doctors to effectively diagnose patients.</p>
-              <div class="flex justify-center lg:justify-start mt-10">
-                <RouterLink to="/join">
-                <Button @click="handleJoin">Join Room</Button>
-                </RouterLink>
-                <Button variant="secondary" @click="handleHost">Host Room</Button>
-              </div>
-          </div>
-          <div class="relative order-1 lg:order-none mb-20 lg:mt-0 lg:mb-0 lg:-mr-10">
-              <img class="z-10 w-full" src="../assets/doctor.png" />
-              <div class="-z-10 bg-theme-primary h-52 w-full sm:h-80 sm:w-full rounded-l-full absolute -right-28 md:-right-48 -bottom-8"></div>
-          </div>
-      </section>
+  <Navbar currentSection="home" />
+  <div class="container mx-auto px-5 overflow-x-hidden">
+    <section class="grid grid-cols-none lg:grid-cols-2 pb-16 pt-8 items-center">
+      <div class="lg:w-5/6 order-2 lg:order-none">
+        <h1 class="text-4xl xl:text-5xl font-bold font-theme-heading text-center lg:text-left">
+          Welcome to Cigna Telehealth!
+        </h1>
+        <p
+          class="lg:text-xl mt-10 font-theme-content text-theme-grayish-blue text-center lg:text-left">
+          A communication tool for doctors to effectively diagnose patients.
+        </p>
+        <div class="flex justify-center lg:justify-start mt-10 space-x-2">
+          <RouterLink to="/join">
+            <Button @click="handleJoin">Join Room</Button>
+          </RouterLink>
+          <Button variant="secondary" @click="handleHost">Host Room</Button>
+        </div>
+      </div>
+      <div class="relative order-1 lg:order-none mb-20 lg:mt-0 lg:mb-0 lg:-mr-10">
+        <img class="z-10 w-full" src="../assets/doctor.png" />
+        <div
+          class="-z-10 bg-theme-primary h-52 w-full sm:h-80 sm:w-full rounded-l-full absolute -right-28 md:-right-48 -bottom-8"></div>
+      </div>
+    </section>
   </div>
 </template>
-../lib/store
